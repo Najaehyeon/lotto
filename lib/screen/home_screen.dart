9102b9cb313989f1bd64b6ac628abd143cdc149a
jaehyeon <jaehyeon4832@gmail.com>
2026-01-18 21:11:29 +0900

    리팩토링

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<int> lottoNumbers = [0, 0, 0, 0, 0, 0];
-  int scriptNumber = 1;
+  int scriptNumber = 0;
 
   void getLottoNumbers() {
     Random random = Random();
@@ -40,84 +40,13 @@ class _HomeScreenState extends State<HomeScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // 이미지
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 64, bottom: 32),
-                child: Image.asset(
-                  "assets/images/grandparents.png",
-                  width: 200,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
+            _Image(),
+            SizedBox(height: 8),
             // 대사
-            SizedBox(
-              width: 300,
-              height: 100,
-              child: Center(
-                child: FittedBox(
-                  fit: BoxFit.scaleDown, // 부모보다 클 때만 크기를 줄임
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    josoangMessages[scriptNumber],
-                    style: TextStyle(
-                      fontSize: 32, // 이 크기가 최대값이 됨
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
+            _Script(scriptNumber: scriptNumber),
+            SizedBox(height: 8),
             // 번호들
-            Expanded(
-              child: GridView(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                padding: EdgeInsets.only(right: 20, left: 20, bottom: 20),
-                children: lottoNumbers.map(
-                  (e) {
-                    Color color = primaryColor;
-                    if (e / 10 >= 4) {
-                      color = fortiethColor;
-                    } else if (e / 10 >= 3) {
-                      color = thirtiethColor;
-                    } else if (e / 10 >= 2) {
-                      color = twentiethColor;
-                    } else if (e / 10 >= 1) {
-                      color = tenthColor;
-                    } else {
-                      color = primaryColor;
-                    }
-                    return Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Container(
-                        alignment: AlignmentGeometry.center,
-                        decoration: BoxDecoration(
-                          color: color,
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        child: Text(
-                          e.toString(),
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ).toList(),
-              ),
-            ),
+            _Numbers(lottoNumbers: lottoNumbers),
             // 버튼
             Column(
               children: [
@@ -140,11 +69,115 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ],
             ),
-            SizedBox(
-              height: 24,
-            ),
+            SizedBox(height: 24),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 64, bottom: 32),
+        child: Image.asset(
+          "assets/images/grandparents.png",
+          width: 200,
+        ),
+      ),
+    );
+  }
+}
+
+class _Script extends StatelessWidget {
+  const _Script({
+    super.key,
+    required this.scriptNumber,
+  });
+
+  final int scriptNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      height: 100,
+      child: Center(
+        child: FittedBox(
+          fit: BoxFit.scaleDown, // 부모보다 클 때만 크기를 줄임
+          child: Text(
+            textAlign: TextAlign.center,
+            josoangMessages[scriptNumber],
+            style: TextStyle(
+              fontSize: 32, // 이 크기가 최대값이 됨
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Numbers extends StatelessWidget {
+  const _Numbers({
+    super.key,
+    required this.lottoNumbers,
+  });
+
+  final List<int> lottoNumbers;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        padding: EdgeInsets.only(right: 20, left: 20, bottom: 20),
+        children: lottoNumbers.map(
+          (e) {
+            Color color = primaryColor;
+            if (e / 10 >= 4) {
+              color = fortiethColor;
+            } else if (e / 10 >= 3) {
+              color = thirtiethColor;
+            } else if (e / 10 >= 2) {
+              color = twentiethColor;
+            } else if (e / 10 >= 1) {
+              color = tenthColor;
+            } else {
+              color = primaryColor;
+            }
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                alignment: AlignmentGeometry.center,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Text(
+                  e.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          },
+        ).toList(),
       ),
     );
   }
